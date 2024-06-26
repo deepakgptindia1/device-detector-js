322d872089723dadd69dd4cbfe9626d6d6723352
Deepak Kumar Gupta <deepkumar@Deepaks-MacBook-Pro.local>
2024-06-26 23:14:27 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 import DeviceDetector from "device-detector-js";
 
+const deviceDetector = new DeviceDetector();
+
 function App() {
-  const [state, setState] = useState({});
-  const date = new Date().toString();  
+  const [userAgent, setUserAgent] = useState(undefined);
+  const [device, setDevice] = useState(undefined);
 
   useEffect(() => {
-    const deviceDetector = new DeviceDetector();
-    const userAgent =
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36";
-    const device = deviceDetector.parse(userAgent);
-    setState(device);
+    if (window.location.hash) {
+      const userAgent = decodeURIComponent(window.location.hash.substr(1));
+
+      return parseUserAgent(userAgent);
+    }
+
+    parseUserAgent(navigator.userAgent);
   }, []);
+
+  const parseUserAgent = (userAgentVal) => {
+    if (userAgentVal.trim() === (userAgent || "").trim()) {
+      return setUserAgent({ userAgent });
+    }
+
+    const device = deviceDetector.parse(userAgentVal);
+
+    setUserAgent(userAgentVal);
+    setDevice(device);
+  };
 
   return (
     <div className="App">
-      <p>{date}</p> 
-
       <h1>Device Details</h1>
-      
-      {JSON.stringify(new DeviceDetector().parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36"))}
+
+      {JSON.stringify(device)}
     </div>
   );
 }

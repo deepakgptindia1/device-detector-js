55e5d1ffca47d146095fa89fb3471282e9cf83e5
Deepak Kumar Gupta <deepkumar@Deepaks-MacBook-Pro.local>
2024-06-26 19:36:10 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import DeviceDetector from "device-detector-js";
 
 function App() {
-  const deviceDetector = new DeviceDetector();
-  const userAgent =
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36";
-  const device = deviceDetector.parse(userAgent);
-  console.log(device);
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    const deviceDetector = new DeviceDetector();
+    const userAgent =
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36";
+    const device = deviceDetector.parse(userAgent);
+    setState(device);
+  }, []);
 
   return (
     <div className="App">
-      <h1></h1>
-      <h2>Device details</h2>
-      <p>Type: {device.device.type}</p>
-      <p>Brand: {device.device.brand}</p>
-      <p>Model: {device.device.model}</p>
-      {device.device.model}
+      <h1>Device Details</h1>
+      {state.device ? (
+        <>
+          <p>Type: {state.device.type}</p>
+          <p>Brand: {state.device.brand}</p>
+          <p>Model: {state.device.model}</p>
+        </>
+      ) : (
+        <p>Loading device details...</p>
+      )}
     </div>
   );
 }

e042d33aa14de1d4751de4630895a32a175db735
Deepak Kumar Gupta <deepkumar@Deepaks-MacBook-Pro.local>
2024-06-26 18:02:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import DeviceDetector from "device-detector-js";
 
 function App() {
+  const deviceDetector = new DeviceDetector();
+  const userAgent =
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36";
+  const device = deviceDetector.parse(userAgent);
+  console.log(device);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1></h1>
+      <h2>Device details</h2>
+      <p>Type: {device.device.type}</p>
+      <p>Brand: {device.device.brand}</p>
+      <p>Model: {device.device.model}</p>
+      {device.device.model}
     </div>
   );
 }
